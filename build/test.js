78bcc36e4ae8b9d8c2091ce69e340a92781aec9d
Corey Hart <corey@codenothing.com>
2013-03-13 09:27:55 -0700

    -Restructuring for new MUnit

--- build/test.js
+++ build/test.js
@@ -1,1 +1,11 @@
-require( 'munit' ).render( __dirname + '/../test/' );
+global.MUnit = require( 'MUnit' );
+global.Nodelint = require( '../' );
+
+// Defaults
+MUnit.extend( MUnit.Defaults.Settings, {
+	stopOnFail: true,
+	timeout: 5000
+});
+
+// Render all tests
+MUnit.render( __dirname + '/../test/' );
