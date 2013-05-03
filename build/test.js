9dee2004e75c52a0392b2d5164054bdc5d56b3a9
Corey Hart <corey@codenothing.com>
2013-05-02 21:24:07 -0700

    Upgrading munit package

--- build/test.js
+++ build/test.js
@@ -1,11 +1,13 @@
 global.munit = require( 'munit' );
 global.Nodelint = require( '../' );
 
-// Kill tests on failure
-munit.extend( munit.defaults.settings, {
-	stopOnFail: true,
-	timeout: 5000
-});
+// Extend module timeout grace period to 5 seconds
+munit.defaults.settings.timeout = 5000;
+
+// Only stop test suite when running make test
+if ( ! process.env.NODE_TEST_NO_SKIP ) {
+	munit.defaults.settings.stopOnFail = true;
+}
 
 // Render all tests
 munit.render( __dirname + '/../test/', {
